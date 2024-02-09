6ab584e8b4bcbd146e4423cca674c65f433013ad
Vadim Godunko <vgodunko@gmail.com>
2024-02-09 23:15:10 +0300

    Root package and base types for SVD.

---
+++ source/a0b-types-svd.ads
@@ -0,0 +1,49 @@
+--
+--  Copyright (C) 2024, Vadim Godunko
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+package A0B.Types.SVD is
+
+   pragma Pure;
+
+   subtype Bit  is A0B.Types.Unsigned_1;
+   subtype Byte is A0B.Types.Unsigned_8;
+
+   subtype UInt1  is A0B.Types.Unsigned_1;
+   subtype UInt2  is A0B.Types.Unsigned_2;
+   subtype UInt3  is A0B.Types.Unsigned_3;
+   subtype UInt4  is A0B.Types.Unsigned_4;
+   subtype UInt5  is A0B.Types.Unsigned_5;
+   subtype UInt6  is A0B.Types.Unsigned_6;
+   subtype UInt7  is A0B.Types.Unsigned_7;
+   subtype UInt8  is A0B.Types.Unsigned_8;
+   subtype UInt9  is A0B.Types.Unsigned_9;
+   subtype UInt10 is A0B.Types.Unsigned_10;
+   subtype UInt11 is A0B.Types.Unsigned_11;
+   subtype UInt12 is A0B.Types.Unsigned_12;
+   subtype UInt13 is A0B.Types.Unsigned_13;
+   subtype UInt14 is A0B.Types.Unsigned_14;
+   subtype UInt15 is A0B.Types.Unsigned_15;
+   subtype UInt16 is A0B.Types.Unsigned_16;
+   subtype UInt17 is A0B.Types.Unsigned_17;
+   subtype UInt18 is A0B.Types.Unsigned_18;
+   subtype UInt19 is A0B.Types.Unsigned_19;
+   subtype UInt20 is A0B.Types.Unsigned_20;
+   subtype UInt21 is A0B.Types.Unsigned_21;
+   subtype UInt22 is A0B.Types.Unsigned_22;
+   subtype UInt23 is A0B.Types.Unsigned_23;
+   subtype UInt24 is A0B.Types.Unsigned_24;
+   subtype UInt25 is A0B.Types.Unsigned_25;
+   subtype UInt26 is A0B.Types.Unsigned_26;
+   subtype UInt27 is A0B.Types.Unsigned_27;
+   subtype UInt28 is A0B.Types.Unsigned_28;
+   subtype UInt29 is A0B.Types.Unsigned_29;
+   subtype UInt30 is A0B.Types.Unsigned_30;
+   subtype UInt31 is A0B.Types.Unsigned_31;
+   subtype UInt32 is A0B.Types.Unsigned_32;
+
+end A0B.Types.SVD;
