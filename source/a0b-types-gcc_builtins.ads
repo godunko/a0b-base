945cf35536a8849fa20afe86f5a3d1c7f7140e73
Vadim Godunko <vgodunko@gmail.com>
2024-03-22 10:59:50 +0400

    Add binding of clz builtin function.

--- source/a0b-types-gcc_builtins.ads
+++ source/a0b-types-gcc_builtins.ads
@@ -14,5 +14,12 @@ package A0B.Types.GCC_Builtins is
 
    function ffs (Item : A0B.Types.Integer_32) return A0B.Types.Integer_32
      with Import, Convention => Intrinsic, External_Name => "__builtin_ffs";
+   --  Returns one plus the index of the least significant 1-bit of x, or if
+   --   x is zero, returns zero.
+
+   function clz (Item : A0B.Types.Unsigned_32) return A0B.Types.Integer_32
+     with Import, Convention => Intrinsic, External_Name => "__builtin_clz";
+   --  Returns the number of leading 0-bits in x, starting at the most
+   --  significant bit position. If x is 0, the result is undefined.
 
 end A0B.Types.GCC_Builtins;
