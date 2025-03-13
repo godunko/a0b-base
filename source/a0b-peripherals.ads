e93840399d90145878ab806091ab4af51314d652
Vadim Godunko <vgodunko@gmail.com>
2025-03-13 21:02:31 +0400

    Root package for MCU's peripherals' definitions.

---
+++ source/a0b-peripherals.ads
@@ -0,0 +1,13 @@
+--
+--  Copyright (C) 2025, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+--  Root package for MCU specific peripherals' definitions.
+
+package A0B.Peripherals
+  with Preelaborate, No_Elaboration_Code_All
+is
+
+end A0B.Peripherals;
