43e78a7d17bc4ab1556db3edc5ed175d94caabcb
Vadim Godunko <vgodunko@gmail.com>
2025-03-15 09:59:47 +0400

    Make package Pure

--- source/a0b-peripherals.ads
+++ source/a0b-peripherals.ads
@@ -7,7 +7,7 @@
 --  Root package for MCU specific peripherals' definitions.
 
 package A0B.Peripherals
-  with Preelaborate, No_Elaboration_Code_All
+  with Pure, No_Elaboration_Code_All
 is
 
 end A0B.Peripherals;
