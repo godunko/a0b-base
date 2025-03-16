be30c2899a8c7aca8a0122a66c96c6981eeccb7d
Vadim Godunko <vgodunko@gmail.com>
2025-03-16 11:06:10 +0400

    Remove type declaration...
    ... it is moved to `a0b-asynchronous_operations` crate

--- source/a0b.ads
+++ source/a0b.ads
@@ -1,5 +1,5 @@
 --
---  Copyright (C) 2024, Vadim Godunko
+--  Copyright (C) 2024-2025, Vadim Godunko
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
@@ -7,12 +7,5 @@
 package A0B
   with Pure, No_Elaboration_Code_All
 is
-
-   type Operation_Status is
-     (Active,    --  Active
-      Success,   --  Completed successfully
-      Failure);  --  Failed
-   --  Status of the requested operation.
-   --  It is declared here to make it available for all drivers.
 
 end A0B;
